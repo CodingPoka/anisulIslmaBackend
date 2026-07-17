f38eedc8625150af562144ce9c736b1c3431f1a5
CodingPoka <kingshukbd.01@gmail.com>
2026-07-17 14:14:52 +0600

    creating loing and resigter router

--- withoutDatabaseCurd/app.js
+++ withoutDatabaseCurd/app.js
@@ -8,6 +8,13 @@ app.get("/",(req,res)=>{
     res.send("Welcome to my first express js server");
 })
 
+app.get("/login",(req,res)=>{
+    res.send("This is a login page");
+})
+
+app.get("/register",(req,res)=>{
+    res.send("This is a register page");
+})
 
 app.post("/",(req,res)=>{
     res.send("This is a post request");
