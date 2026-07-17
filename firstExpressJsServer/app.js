dae5bafe2586cf77d1bf42abfb3afc936d1742ff
CodingPoka <kingshukbd.01@gmail.com>
2026-07-17 14:00:10 +0600

    in the app.js create about route

--- firstExpressJsServer/app.js
+++ firstExpressJsServer/app.js
@@ -2,7 +2,12 @@
 const express = require('express');
 const app = express();
 
+app.get("/",(req,res)=>{
+    res.send("Welcome to my first express js server");
+})
 
-
+app.get("/about",(req,res)=>{
+    res.send("This is about page");
+})
 
 module.exports = app;
--- firstExpressJsServer/index.js
+++ firstExpressJsServer/index.js
@@ -4,9 +4,7 @@ const app=require("./app.js");
 const port = 3000;
 
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to my first express js server");
-})
+
 
 app.listen(port,()=>{
     console.log(`Server is running successfully on http://localhost:${port} `);
